24e24d3e5b44f1801f31ef9a08afa2e3bf1e8ca5
Wenchu Du <duwenchu999@gmail.com>
2019-08-24 18:00:10 +1000

    Initial commit

---
+++ public/js/age.js
@@ -0,0 +1,42 @@
+ var option = {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            type: 'shadow'
+        }
+    },
+    legend: {
+        data: ['Rural', 'Urban']
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: {
+        type: 'value',
+        boundaryGap: [0, 0.01]
+    },
+    yAxis: {
+        type: 'category',
+        data: ['65+','55-65','45-54','35-44','25-34','18-24']
+    },
+    series: [
+        {
+            name: 'Rural',
+            type: 'bar',
+            data: [21.36, 25.11, 21.09, 21.88, 34.14, 28.94]
+        },
+        {
+            name: 'Urban',
+            type: 'bar',
+            data: [17.86, 20.2, 19.18, 19.76, 27.47, 26.79]
+        }
+    ]
+};
+
+var myChart = echarts.init(document.getElementById('wrap'));
+
+//使用制定的配置项和数据显示图表
+myChart.setOption(option);
