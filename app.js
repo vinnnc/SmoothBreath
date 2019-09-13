eb3097f32fa955127562f9267f525da074e0f030
Wenchu Du <duwenchu999@gmail.com>
2019-09-13 16:08:29 +1000

    Handled custom page not found

--- app.js
+++ app.js
@@ -29,13 +29,14 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // // set locals, only providing error in development
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //
+  // // render the error page
+  // res.status(err.status || 500);
+  res.sendFile(path.join(__dirname + '/public/error.html'));
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
